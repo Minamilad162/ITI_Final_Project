5cb1bdce45c1d3b5d6fc4db3298c1b37bcb5994e
Mina Milad <minamilad939@gmail.com>
2021-04-14 05:43:05 +0200

    Accordion modification

--- src/components/Landing-Page/accordion.js
+++ src/components/Landing-Page/accordion.js
@@ -16,12 +16,12 @@ export default function Accordion() {
                             aria-expanded="false"
                             aria-controls="flush-collapseOne"
                         >
-                            <span className="span">What Is Netflix ?</span>
+                            <span className="span">What Is CinemaFy ?</span>
                         </button>
                     </h2>
                     <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body bb">
-                            Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You
+                            Cinemafy is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You
                             can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and
                             movies are added every week!
                         </div>
@@ -37,12 +37,12 @@ export default function Accordion() {
                             aria-expanded="false"
                             aria-controls="flush-collapseTwo"
                         >
-                            <span className="span">How much Netflix Coast ?</span>
+                            <span className="span">How much Cinemafy Coast ?</span>
                         </button>
                     </h2>
                     <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body bb">
-                            Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from EGP120 to EGP200 a month. No extra costs,
+                            Watch Cinemafy on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from EGP120 to EGP200 a month. No extra costs,
                             no contracts
                         </div>
                     </div>
@@ -62,9 +62,9 @@ export default function Accordion() {
                     </h2>
                     <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body bb">
-                            Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on
-                            any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your
-                            favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
+                            Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Cinemafy account to watch instantly on the web at Cinemafy.com from your personal computer or on
+                            any internet-connected device that offers the Cinemafy app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your
+                            favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Cinemafy with you anywhere.
                         </div>
                     </div>
                 </div>
@@ -83,7 +83,7 @@ export default function Accordion() {
                     </h2>
                     <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body bb">
-                            Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or
+                            Cinemafy is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or
                             stop your account anytime..
                         </div>
                     </div>
@@ -98,12 +98,12 @@ export default function Accordion() {
                             aria-expanded="false"
                             aria-controls="flush-collapseFive "
                         >
-                            <span className="span">What Can I watch on Netflix</span>
+                            <span className="span">What Can I watch on Cinemafy</span>
                         </button>
                     </h2>
                     <div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body bb">
-                            Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want
+                            Cinemafy has an extensive library of feature films, documentaries, TV shows, anime, award-winning Cinemafy originals, and more. Watch as much as you want, anytime you want
                         </div>
                     </div>
                 </div>
